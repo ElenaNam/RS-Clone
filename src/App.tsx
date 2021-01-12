066c0e2e32f0dc3873ab2a0cc64780f296bd3960
Nickita Solovei <nickaya112@gmail.com>
2021-01-12 22:43:07 +0300

    feat: add react app

---
+++ src/App.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+// import logo from './logo.svg';
+import './App.css';
+import HeaderMainMenu from './components/header-main-menu/header-main-menu';
+
+function App() {
+  return (
+    <div className="App">
+      <HeaderMainMenu />
+      <header className="App-header">
+
+        {/* <div className="first-table">jh</div> */}
+        {/* <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a> */}
+      </header>
+    </div>
+  );
+}
+
+export default App;
